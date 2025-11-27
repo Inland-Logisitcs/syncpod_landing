149d351fbf2541e5c8149cb2bf119ee28ca99920
widrunner <widvin.diaz@inlandlogistics.co>
2025-11-27 11:35:44 -0600

    fixed the next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   basePath :  "/syncpod_landing/",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  assetPrefix: "/syncpod_landing/", // You need to change this
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
