6a5f7aa39b2d9f3d43dd05f09dd10ab9e33f34c0
widrunner <widvin.diaz@inlandlogistics.co>
2025-11-28 09:54:56 -0600

    change in the export output

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath :  "/syncpod_landing",
-  output: "export",  // <=== enables static exports
+  //output: "export",  // <=== enables static exports
   reactStrictMode: true,
   assetPrefix: "/syncpod_landing", // You need to change this
   i18n: {
