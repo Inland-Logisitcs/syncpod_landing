bf74d4e9856611ecf00ef1f4400b774d6cc35634
widrunner <widvin.diaz@inlandlogistics.co>
2025-12-01 17:36:52 -0600

    changes to develop

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/syncpod_landing/', // replace with your GitHub repo name
+  // other configs
+
 })
